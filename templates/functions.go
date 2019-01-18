27d0ac09b57dfbdfa337a2aaa7f808606b97f78b
Leonel Quinteros <leonel.quinteros@gmail.com>
2019-01-18 16:40:01 -0300

    Add BuildID function

--- templates/functions.go
+++ templates/functions.go
@@ -8,14 +8,32 @@ import (
 
 // FuncMap is passed to the template object that renders every view
 var FuncMap = template.FuncMap{
-	"ID": ID,
+	"ID":      ID,
+	"BuildID": BuildID,
 }
 
-// ID returns a random [8]byte value encoded as hex string.
+// ID returns a random [8]byte value encoded as hex string (16).
 func ID() string {
 	id, err := gorand.GetHex(8)
 	if err != nil {
-		return "0"
+		return "0123456789abcdef"
 	}
 	return id
+}
+
+var buildID string
+
+func init() {
+	// Generate buildID
+	id, err := gorand.GetHex(8)
+	if err != nil {
+		buildID = "0123456789abcdef"
+	} else {
+		buildID = id
+	}
+}
+
+// BuildID returns a randomly-generated-at-runtime constant value
+func BuildID() string {
+	return buildID
 }
